6d77aa4e8fcd4ba6b3c00f10197d76ef696e9924
Brian Foley <brian.foley1@montana.edu>
2022-12-30 11:57:32 -0700

    Adding layout and components

--- app.vue
+++ app.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
-    <h1 class="bg-blue-200 h-[600px] scss-style">Welcome to the homepage</h1>
-    <AppAlert>
-      This is an auto-imported component.
-    </AppAlert>
+    <NuxtLayout name="default">
+      <h1 class="bg-blue-200 h-[600px] scss-style">Welcome to the homepage</h1>
+      <AppAlert>
+        This is an auto-imported component.
+      </AppAlert>
+    </NuxtLayout>
+    
   </div>
 </template>
+
+<script setup>
+definePageMeta({ //This isn't needed but gives you an idea as to how you can use a custom layout or additional layouts
+  layout: "default"
+})
+
+
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>This is a singular post page</h1>
+</template>
